bcff56f94f6046bea8ff9e78ebc4a064a1d6ce7a
yumnashaikh07 <164198271+yumnashaikh07@users.noreply.github.com>
2025-01-06 15:03:22 +0500

    added styling

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -36,8 +36,8 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
 
   return (
     <main className="flex flex-col max-w-5xl items-center p-4 mx-auto ">
-      <h1 className="text-3xl md:text-5xl  my-5 font-semibold">{post.title}</h1>
-      <p className="text-sm mb-4 border-l-2 text-start text-gray-600 pl-3 italic border-gray-500">{post.description}</p>
+      <h1 className="text-3xl md:text-5xl my-5 font-semibold">{post.title}</h1>
+      <p className="text-sm mb-4 border-l-2 mr-52 text-gray-600 pl-3 italic border-gray-500">{post.description}</p>
       <Image src={urlFor(post.image).url()} alt={post.title} width={500} height={500}  className="mb-5"/>
       
       <div className="text-xl ">
@@ -45,7 +45,7 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
       </div>
       <div className="border-b-2 mt-5 w-full border-gray-500 mx-auto"></div>
       <div className="border-b-2 mt-1 w-full border-gray-500 mx-auto"></div>
-      <div className="flex justify-between mx-auto space-x-72 md:space-x-[900px]  mb-20 items-center">
+      <div className="flex justify-between mx-auto space-x-[277px] md:space-x-[900px]  mb-20 items-center">
       <Link href={"/blog"} ><ArrowBigLeft size={47} strokeWidth={1.25} className=" transition-transform duration-500 hover:scale-150 "/></Link>
       <Link href={"/"} ><HomeIcon size={35} strokeWidth={1.5} className=" transition-transform duration-500 hover:scale-150 "/></Link>
       </div>
